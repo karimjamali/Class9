d41d57cc2d264898dcac8734e6012911fc603051
Karim <karim.jamali@gmail.com>
2016-07-23 07:33:41 -0700

    Updated ex8 and ex9

---
+++ ex7/mytest/mytest/world.py
@@ -0,0 +1,27 @@
+
+def func2():
+  print' Sorry, didnt recognize you'
+
+def main():
+  print 'This is the world module'
+
+class MyClass(object):
+  def __init__(self,a,b,c):
+     self.a = a
+     self.b = b
+     self.c = c
+
+  def Hello(self):
+     print self.a, self.b, self.c
+ 
+  def Not_Hello(self):
+     print 'not' , self.a, self.b, self.c
+class Inherited_Class(MyClass):
+  def __init__(self,a,b,c):
+    print 'Init file with changes for child class'
+    MyClass.__init__(self,a,b,c) 
+  def Hello(self):
+    print 'This is inheritance with overriding', self.a,self.b,self.c
+ 
+if __name__ == '__main__':
+   main()
---
+++ ex8/mytest/ex9.py
@@ -0,0 +1,14 @@
+from mytest import *
+
+def main():
+    print ' Testing func1, func2, func3 from another directory \n' 
+    func1()
+    func2()
+    func3()
+
+
+test_object=MyClass(100,211,322)
+test_object.Hello()
+
+if __name__ =="__main__":
+    main()
